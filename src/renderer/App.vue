9709bba6f327ce1a01f5eadfef6d996cfccb948f
wyh <1074021399@qq.com>
2022-12-27 16:36:13 +0800

    初始化仓库

---
+++ src/renderer/App.vue
@@ -0,0 +1,45 @@
+<!--
+ * @Description:
+ * @Author: 华松林
+ * @Date: 2021-09-24 16:56:34
+ * @LastEditors: 华松林
+ * @LastEditTime: 2021-09-26 10:16:02
+ * @FilePath: \single-reader\src\renderer\App.vue
+-->
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'single-reader'
+  }
+</script>
+
+<style>
+  /* CSS */
+  html, body, #app {
+    height: 100%;
+  }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+  /**滚动条的宽度*/
+  ::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+  }
+
+  /*滚动条的滑块*/
+  ::-webkit-scrollbar-thumb {
+    background-color: rgba(0,0,0,.2);
+    border-radius: 5px;
+  }
+</style>
---
+++ src/renderer/pages/index.vue
@@ -0,0 +1,190 @@
+<!--
+import { MenuItem } from 'electron';
+import { log } from 'util';
+ * @Description:
+ * @Author: 华松林
+ * @Date: 2021-09-24 17:05:35
+ * @LastEditors: 华松林
+ * @LastEditTime: 2021-09-27 11:11:05
+ * @FilePath: \single-reader\src\renderer\pages\index.vue
+-->
+<template>
+  <div id="page" :class="{active: mouseInView && isShow}"  @dblclick="showText" @mouseover.prevent="mouseIn(true)" @mouseout.prevent="mouseIn(false)" @contextmenu.prevent="rightClick">
+    <!-- 如果鼠标不再阅读器上则显示随便放一个网站域名 -->
+    <div class="preview">
+      <!--{{ mouseInView ? nowRead : 'fanyi.baidu.com/translate?aldtype=16047&query=&keyfrom=baidu&smartresult=dict&lang=auto2zh#en/zh/single-reader' }}-->
+      {{(mouseInView && isShow) ? nowRead : ''}}
+      {{ book && nowRead.length < lineSize ? '（已读完）' : '' }}
+    </div>
+    <!-- {{ keyword }} -->
+  </div>
+</template>
+
+<script>
+  import { ipcRenderer, app } from "electron";
+  const { remote } = require('electron')
+  const mainWindow = remote.getCurrentWindow()
+  export default {
+    name: 'index-page',
+    data: () => {
+      return {
+        content: ``,
+        lineSize: 300, // 一行显示38个字
+        mouseInView: false,
+        menu: null,
+        keyword: '',
+        isShow:false,
+        isLoading:false,
+        isLoading2:false
+      }
+    },
+    mounted() {
+
+    },
+    created() {
+      // 监听键盘事件
+      document.onkeydown = (e) => {
+        this.keyupEvent(e)
+      }
+      this.initMenu()
+	    // window.addEventListener('scroll', this.Scrollbottom);  //页面加载时监听滚动事件
+    },
+    computed: {
+      book() {
+        return this.$store.state.Book.book
+      },
+      // 正在阅读的内容
+      nowRead() {
+        return (this.book && this.book.context) ? this.book.context.slice(this.lineSize * (this.book.page - 1), this.lineSize * this.book.page) : '请先右键选择'
+      }
+    },
+    methods: {
+      // 鼠标是否放置在阅读器中
+      mouseIn(value) {
+        this.mouseInView = value
+        if(!value){
+        	this.isShow = false
+	        mainWindow.setOpacity(0.02)
+	        document.body.style.background = '#DEE1E6'
+        }
+      },
+
+// methods中
+	    Scrollbottom() {
+		    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		    let clientHeight = document.documentElement.clientHeight;
+		    let scrollHeight = document.documentElement.scrollHeight;
+		    // console.log("",scrollTop)
+		    if (scrollTop + clientHeight >= scrollHeight) {
+		    	if(this.isLoading){
+		    		clearTimeout(this.isLoading)
+            this.isLoading = null
+		    	}
+		    	this.isLoading = setTimeout(()=>{
+				    console.log("滚动到底部了")
+				    // this.book.page+=1
+				    document.documentElement.scrollTop = 10;
+				    document.body.scrollTop = 10;
+				    this.$store.dispatch('nextPage')
+          },500)
+			    // this.pageNo++
+			    // console.log(this.pageNo);
+			    // this.fetchData()
+		    }
+		    if(scrollTop <= 0){
+			    if(this.isLoading2){
+			    	clearTimeout(this.isLoading2)
+				    this.isLoading2 = null
+			    }
+			    this.isLoading2 = setTimeout(()=>{
+				    document.documentElement.scrollTop = 10;
+				    document.body.scrollTop = 10;
+				    this.$store.dispatch('prevPage')
+			    },500)
+        }
+	    },
+      //双击内容区域展示
+	    showText(){
+      	this.isShow = true
+		    mainWindow.setOpacity(.8)
+        document.body.style.background = '#333'
+      },
+      // 按键
+      keyupEvent(e) {
+      	console.log(window.event.keyCode)
+        if (!this.mouseInView) return
+
+        switch (window.event.keyCode) {
+          case 37:
+            // 上一页
+            this.prevPage()
+            break;
+          case 39:
+            // 下一页
+            this.nextPage()
+            break;
+          case 65:
+            // 上一页
+            this.prevPage()
+            break;
+          case 68:
+            // 下一页
+            this.nextPage()
+            break;
+        }
+      },
+      prevPage() {
+        this.$store.dispatch('prevPage')
+      },
+      nextPage() {
+        // 如果当前正在阅读的内容小于一行显示的文本数量，则说明是最后一行
+        if (this.nowRead.length === this.lineSize) {
+          this.$store.dispatch('nextPage')
+        }
+      },
+      // 初始化菜单
+      initMenu() {
+        const Menu = remote.Menu
+        const MenuItem = remote.MenuItem
+        this.menu = new Menu()
+        this.menu.append(new MenuItem({
+          label: '菜单',
+          click: () => {
+            ipcRenderer.send('openMenuWindow')
+          }
+        }))
+      },
+      // 右键打开菜单
+      rightClick() {
+        this.menu.popup(remote.getCurrentWindow())
+      }
+    }
+  }
+</script>
+
+<style>
+  #page {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    /*-webkit-app-region: drag;*/
+    -webkit-app-region: no-drag;
+  }
+  .active{
+    color: #ddd;
+    /*background: rgba(0,0,0,.8);*/
+  }
+  .preview {
+    width: 90%;
+    /*height: 150%;*/
+    margin: 20px auto;
+    padding: 20px;
+    line-height: 23px;
+    font-size: 13px;
+    /*background: rgba(0,0,0,.8);*/
+    /*background-color: #fff;*/
+    -webkit-app-region: drag;
+    /*-webkit-app-region: no-drag;*/
+
+  }
+</style>
